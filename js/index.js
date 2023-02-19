d59eadbeac9e4c5e32c83824145dde1dadbf7af8
LucienL <ravagnier-lucas@gmail.com>
2023-02-19 01:58:43 +0100

    [UPDATE]
    - Mise à jour de la page d'acceuil

---
+++ js/index.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function () {
+    // typing animation
+    (function ($) {
+    $.fn.writeText = function (content) {
+      var contentArray = content.split(""),
+        current = 0,
+        elem = this;
+      setInterval(function () {
+        if (current < contentArray.length) {
+          elem.text(elem.text() + contentArray[current++]);
+        }
+      }, 80);
+    };
+  })(jQuery);
+
+  // input text for typing animation
+  $("#description").writeText("WEB DEVELOPPER + DATABASE ADMINISTRATOR");
+});
